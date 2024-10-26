37c0b6d411b94920bfa1f83845b6b20d84ffdef8
jin-bob <bobyevhenii@gmail.com>
2024-10-26 16:16:15 +0300

    googleAuth: added basic google auth * added deploy action * refactored .gitignore

---
+++ src/pages/auth/index.js
@@ -0,0 +1,22 @@
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import scopes from "constants/googleAuthScopes";
+
+export default function Auth() {
+  return (
+    <div>
+      <div>Auth</div>
+
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <div>
+          <GoogleLogin
+            buttonText="Login with Google"
+            cookiePolicy="single_host_origin"
+            scope={scopes.join(" ")}
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("Login Failed")}
+          />
+        </div>
+      </GoogleOAuthProvider>
+    </div>
+  );
+}
