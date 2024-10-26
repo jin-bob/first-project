3486ea1708f1d0dcb94046e4489d4f58b5f96043
jin-bob <bobyevhenii@gmail.com>
2024-10-26 12:56:09 +0300

    routing: installed prop-types * added LoadingSuspense component * added base LandingPage * added routing file * added RootElement * refactored App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import router from "./routing";
 import queryClient from "./queryClient";
 import packageJSON from "../package.json";
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>Hello World!</div>
+      <RouterProvider router={router} />
 
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
 
